28ccf1cfd85e412725751e330065f0352774a4bc
Kristian Barrett <kba@firstagenda.com>
2019-07-30 19:49:13 +0200

    Fixed the unit test that ensure correct methods are called from billing service

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -2,31 +2,36 @@ package io.pleo.antaeus.core.services
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.data.AntaeusDal
 import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class BillingServiceTest {
-    private val invoice = Invoice(1, 1, Money(10.toBigDecimal(), Currency.EUR), InvoiceStatus.PENDING)
+    private val pendingInvoice = Invoice(1, 1, Money(10.toBigDecimal(), Currency.EUR), InvoiceStatus.PENDING)
 
     private val dal = mockk<AntaeusDal> {
-        every { fetchInvoices() } returns listOf(invoice)
+        every { fetchInvoices() } returns listOf(pendingInvoice)
     }
 
     private val paymentProvider = mockk<PaymentProvider> {
-        every { charge(invoice) } returns true
+        every { charge(any()) } returns true
     }
 
     private val sut = BillingService(dal = dal, paymentProvider = paymentProvider)
 
     @Test
-    fun `will change invoice to paid`() {
+    fun `will call dal to set status to paid`() {
         sut.payAllInvoices()
-        Assertions.assertEquals(InvoiceStatus.PAID, invoice.status)
+        verify(exactly = 1) {
+            paymentProvider.charge(pendingInvoice)
+        }
+        verify(exactly = 1) {
+            dal.updateInvoice(pendingInvoice.id, InvoiceStatus.PAID)
+        }
     }
 }
