87ff53140a5f0e17ccf59c2bf4def904663e07e2
Kristian Barrett <kba@firstagenda.com>
2019-07-30 19:48:42 +0200

    Added code to the update invoice method

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -12,10 +12,7 @@ import io.pleo.antaeus.models.Customer
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class AntaeusDal(private val db: Database) {
@@ -82,6 +79,10 @@ class AntaeusDal(private val db: Database) {
     }
 
     fun updateInvoice(id: Int, paid: InvoiceStatus) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        transaction (db) {
+            InvoiceTable.update ({ InvoiceTable.id eq id }) {
+                it[InvoiceTable.status] = paid.toString()
+            }
+        }
     }
 }
